6ef3385eba115d71395c05aa84e4ec9f0590e1e2
theandrunique <theandrunique@gmail.com>
2024-02-22 16:35:52 +0300

    Update example

--- refresh_tokens_example.py
+++ refresh_tokens_example.py
@@ -18,6 +18,7 @@ app = FastAPI()
 SECRET_KEY = "381fe4a2683cd0eee27cd66bfe1e5b02142ab7ee64d4f1ccbf1011e7358b005e"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 10
+REFRESH_TOKEN_EXPIRE_MINUTES = 24 * 60
 
 
 oauth2_scheme = OAuth2PasswordBearer(
@@ -26,30 +27,35 @@ oauth2_scheme = OAuth2PasswordBearer(
 )
 
 
+def hash_password(password: str) -> bytes:
+    return bcrypt.hashpw(
+        password=password.encode(),
+        salt=bcrypt.gensalt(),
+    )
+
+
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": b"$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "hashed_password": hash_password("12345"),
+        "refresh_token": None,
         "active": True,
     },
     "alice": {
         "username": "alice",
         "full_name": "Alice Chains",
         "email": "alicechains@example.com",
-        "hashed_password": b"$2b$12$gSvqqUPvlXP2tfVFaWK1Be7DlH.PKZbv5H8KnzzVgXXbVxpva.pFm",
+        "hashed_password": hash_password("12345"),
+        "refresh_token": None,
         "active": True,
     },
 }
 
 
-class RefreshToken(BaseModel):
+class Token(BaseModel):
     refresh_token: str
-    token_type: str
-
-
-class AccessToken(BaseModel):
     access_token: str
     token_type: str
 
@@ -70,19 +76,16 @@ class TokenData(BaseModel):
     scopes: list[str] = []
 
 
-# def hash_password(password: str) -> bytes:
-#     return bcrypt.hashpw(
-#         password=password.encode(),
-#         salt=bcrypt.gensalt(),
-#     )
+def update_refresh_token(username: str, token: str):
+    fake_users_db[username]["refresh_token"] = token
 
 
-def create_refresh_token(data: dict, expires_delta: datetime.timedelta):
+def create_token(data: dict, expires_delta: datetime.timedelta, token_type: str):
     encoded_jwt = jwt.encode(
         payload={
             **data,
             "exp": datetime.datetime.now(datetime.timezone.utc) + expires_delta,
-            "token_type": "refresh",
+            "token_type": token_type,
         },
         key=SECRET_KEY,
         algorithm=ALGORITHM,
@@ -90,37 +93,9 @@ def create_refresh_token(data: dict, expires_delta: datetime.timedelta):
     return encoded_jwt
 
 
-def create_access_token(data: dict, expires_delta: datetime.timedelta):
-    encoded_jwt = jwt.encode(
-        payload={
-            **data,
-            "exp": datetime.datetime.now(datetime.timezone.utc) + expires_delta,
-            "token_type": "access",
-        },
-        key=SECRET_KEY,
-        algorithm=ALGORITHM,
-    )
-    return encoded_jwt
-
-
-def validate_access_token(token: str):
-    payload: dict = jwt.decode(
-        jwt=token,
-        key=SECRET_KEY,
-        algorithms=[ALGORITHM],
-    )
-    if payload.get("token_type") != "access":
-        raise PyJWTError()
-    return payload
-
-
-def validate_refresh_token(token: str):
-    payload: dict = jwt.decode(
-        jwt=token,
-        key=SECRET_KEY,
-        algorithms=[ALGORITHM],
-    )
-    if payload.get("token_type") != "refresh":
+def validate_token(token: str, token_type: str):
+    payload: dict = jwt.decode(jwt=token, key=SECRET_KEY, algorithms=[ALGORITHM])
+    if payload.get("token_type") != token_type:
         raise PyJWTError()
     return payload
 
@@ -177,7 +152,7 @@ async def get_current_user(
     )
 
     try:
-        payload: dict = validate_access_token(token=token)
+        payload: dict = validate_token(token=token, token_type="access")
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
@@ -225,12 +200,31 @@ def get_refresh_token(
     token: Annotated[str, Depends(oauth2_scheme)],
 ) -> dict:
     try:
-        return validate_refresh_token(token=token)
+        payload = validate_token(token=token, token_type="refresh")
+        username = payload["sub"]
+        if fake_users_db[username]["refresh_token"] != token:
+            raise PyJWTError()
+        return payload
     except PyJWTError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid token",
         )
+
+
+def create_tokens(data: dict) -> Token:
+    refresh_token = create_token(
+        data=data,
+        expires_delta=datetime.timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES),
+        token_type="refresh"
+    )
+    access_token = create_token(
+        data=data,
+        expires_delta=datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+        token_type="access"
+    )
+    update_refresh_token(username=data["sub"], token=refresh_token)
+    return Token(refresh_token=refresh_token, access_token=access_token, token_type="bearer")
 
 
 @app.post("/auth/")
@@ -246,27 +240,20 @@ def login(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
         )
-    refresh_token = create_refresh_token(
-        data={
-            "sub": user.username,  # it is better to use the user's ID
-            "scopes": form_data.scopes,
-        },
-        expires_delta=datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
-    )
-    return RefreshToken(refresh_token=refresh_token, token_type="bearer")
+    
+    tokens = create_tokens({
+        "sub": user.username,  # it is better to use the user's ID
+        "scopes": form_data.scopes,
+    })
+    return tokens
 
 
 @app.post("/refresh-token/")
 def refresh(payload: dict = Depends(get_refresh_token)):
-    access_token = create_access_token(
-        data={
-            "sub": payload["sub"],
-            "scopes": payload["scopes"],
-        },
-        expires_delta=datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
-    )
-    
-    return AccessToken(access_token=access_token, token_type="bearer")
+    return create_tokens({
+        "sub": payload["sub"],
+        "scopes": payload["scopes"],
+    })
 
 
 @app.post("/introspect/")
