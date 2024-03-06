76c01b8f3abac44f42925c8c2fa10b767e3a7ade
theandrunique <theandrunique@gmail.com>
2024-03-06 18:02:38 +0300

    Refactor structure of project models

--- src/models.py
+++ src/models.py
@@ -1,31 +1,5 @@
-import datetime
-
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     pass
-
-
-class UserInDB(Base):
-    __tablename__ = "users"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True, index=True)
-    email: Mapped[str] = mapped_column(unique=True, index=True)
-    email_verified: Mapped[bool] = mapped_column(default=False)
-    hashed_password: Mapped[bytes]
-    created_at: Mapped[datetime.datetime]
-    active: Mapped[bool] = mapped_column(default=True)
-
-
-class RefreshTokenInDB(Base):
-    __tablename__ = "refresh_tokens"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    jti: Mapped[str]
-    created_at: Mapped[datetime.datetime]
-    last_accessed: Mapped[datetime.datetime]
-    ip_address: Mapped[str | None] = mapped_column(nullable=True)
