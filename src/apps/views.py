1cf164a7c1bbb23e8825d2a30cb06f29cc5e8dbf
theandrunique <theandrunique@gmail.com>
2024-03-16 18:42:44 +0300

    Update apps module

--- src/apps/views.py
+++ src/apps/views.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from fastapi import APIRouter, HTTPException, status
 from pymongo import ReturnDocument
 
+from src.auth.dependencies import UserAuthorization
 from src.mongo_helper import db
 
 from .schemas import AppCreate, AppMongoSchema, AppSchema, AppUpdate
@@ -14,39 +15,57 @@ app_collection = db["apps"]
 
 
 @router.post("/", response_model=AppSchema)
-async def create_app(app: AppCreate):
-    new_app = AppMongoSchema(**app.model_dump(), creator_id=1)
+async def create_app(app: AppCreate, user: UserAuthorization):
+    new_app = AppMongoSchema(**app.model_dump(), creator_id=user.id)
     await app_collection.insert_one(new_app.model_dump(by_alias=True))
     return new_app
 
 
 @router.get("/{app_id}/")
-async def get_app_by_id(app_id: UUID):
-    app = await app_collection.find_one({"_id": app_id})
-    if app is None:
+async def get_app_by_id(app_id: UUID, user: UserAuthorization):
+    found_app = await app_collection.find_one({"_id": app_id})
+    if found_app is None:
         raise HTTPException(status_code=404, detail=f"App {app_id} not found")
-    return AppSchema(**app)
+    app = AppSchema(**found_app)
+    if app.creator_id != user.id:
+        raise HTTPException(status_code=403, detail="Forbidden")
+    return app
 
 
 @router.patch("/{app_id}/")
-async def update_app(app_id: UUID, data: AppUpdate):
+async def update_app(app_id: UUID, data: AppUpdate, user: UserAuthorization):
     new_values = data.model_dump(exclude_defaults=True)
 
+    found_app = await app_collection.find_one({"_id": app_id})
+    if found_app is None:
+        raise HTTPException(status_code=404, detail=f"App {app_id} not found")
+
+    app = AppSchema(**found_app)
+
+    if app.creator_id != user.id:
+        raise HTTPException(status_code=403, detail="Forbidden")
+
+    print("im here")
     if new_values:
         updated_app = await app_collection.find_one_and_update(
             {"_id": app_id},
             {"$set": new_values},
             return_document=ReturnDocument.AFTER,
         )
-        if updated_app is None:
-            raise HTTPException(status_code=404, detail=f"App {app_id} not found")
         return AppSchema(**updated_app)
     else:
-        return AppSchema(**await app_collection.find_one({"_id": app_id}))
+        return app
 
 
 @router.delete("/{app_id}/", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_app(app_id: UUID):
-    delete_result = await app_collection.delete_one({"_id": app_id})
-    if delete_result.deleted_count != 1:
+async def delete_app(app_id: UUID, user: UserAuthorization):
+    found_app = await app_collection.find_one({"_id": app_id})
+    if found_app is None:
         raise HTTPException(status_code=404, detail=f"App {app_id} not found")
+
+    app = AppSchema(**found_app)
+
+    if app.creator_id != user.id:
+        raise HTTPException(status_code=403, detail="Forbidden")
+
+    await app_collection.delete_one({"_id": app_id})
--- src/mongo_helper.py
+++ src/mongo_helper.py
@@ -4,5 +4,6 @@ from src.config import settings
 
 mongo_client = motor.motor_asyncio.AsyncIOMotorClient(
     settings.MONGO_URL,
+    uuidRepresentation='standard'
 )
 db = mongo_client[settings.MONGO_DATABASE_NAME]
