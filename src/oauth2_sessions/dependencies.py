6de0efbfac80e52654756c3f3e13a08025ad001d
theandrunique <theandrunique@gmail.com>
2024-05-18 23:22:45 +0300

    Update oauth sessions dependencies

--- src/oauth2_sessions/dependencies.py
+++ src/oauth2_sessions/dependencies.py
@@ -1,25 +1,21 @@
 from typing import Annotated
-from uuid import UUID
 
 from fastapi import Depends
 
-from src.auth.dependencies import UserAuthorization
 from src.mongo import db
 
 from .repository import OAuth2SessionsRepository
 from .service import OAuth2SessionsService
 
-
-def get_oauth2_sessions_service_by_id(user_id: UUID) -> OAuth2SessionsService:
-    return OAuth2SessionsService(
-        repository=OAuth2SessionsRepository(
-            collection=db[f"oauth2_sessions_{user_id.hex}"],
-        )
+service = OAuth2SessionsService(
+    repository=OAuth2SessionsRepository(
+        collection=db["oauth2_sessions"],
     )
+)
 
 
-async def get_oauth2_sessions_service(user: UserAuthorization) -> OAuth2SessionsService:
-    return get_oauth2_sessions_service_by_id(user.id)
+async def get_oauth2_sessions_service() -> OAuth2SessionsService:
+    return service
 
 
 OAuth2SessionsServiceDep = Annotated[
