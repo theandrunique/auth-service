b9c208787e8c13598b15027de20bd15415fec9f9
theandrunique <theandrunique@gmail.com>
2024-06-14 20:03:27 +0300

    Update AppAuth dependency

--- src/oauth2/dependencies.py
+++ src/oauth2/dependencies.py
@@ -7,25 +7,38 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from src.apps.schemas import AppInMongo
 from src.dependencies import Container, Provide
 
-basic_auth = HTTPBasic()
+from .authoritative_apps import AuthoritativeApp
+from .exceptions import InvalidAppCredentials
+
+basic_auth = HTTPBasic(
+    description="Basic authentication for the app. Use the format `client_id:client_secret`",
+    auto_error=False,
+)
 
 
 async def get_app_auth(
     apps_service=Provide(Container.AppsService),
+    authoritative_apps_service=Provide(Container.AuthoritativeAppsService),
     credentials: HTTPBasicCredentials = Security(basic_auth),
-) -> AppInMongo | None:
+) -> AuthoritativeApp | AppInMongo | None:
+    if not credentials:
+        return None
+
     try:
         client_id = UUID(credentials.username)
         client_secret = UUID(credentials.password)
     except ValueError:
-        return None
+        raise InvalidAppCredentials()
+
+    found = authoritative_apps_service.get_by_client_id(client_id)
+    if found and client_secret == found.client_secret:
+        return found
 
     found = await apps_service.get_by_client_id(client_id)
     if not found:
-        return None
-
+        raise InvalidAppCredentials()
     if client_secret != found.client_secret:
-        return None
+        raise InvalidAppCredentials()
 
     return found
 
